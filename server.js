ad8c591a3e75688b80ed6338e349da08a6261c4f
unknown <anvesh@scienstechnologies.com>
2024-05-17 12:52:35 +0530

    change socket flow

--- server.js
+++ server.js
@@ -2,10 +2,43 @@ const http = require("http");
 const express = require("express");
 const path = require("path");
 const { Server } = require("socket.io");
+const cors = require("cors");
 
 const app = express();
+// const server = http.createServer(app);
+// const io = new Server(server);
+
+// io.on("connection", (socket) => {
+//   socket.on("chating-message", (data) => {
+//     io.emit("message", { name: data.name, message: data.message });
+//     console.log(`${data.name}: ${data.message}`);
+//   });
+// });
+
+// app.use(express.static(path.resolve("./src")));
+
+// app.get("/", (req, res) => {
+//   return res.sendFile("/src/index.html");
+// });
+
+// server.listen(3000, () => console.log(`Server Started at PORT:3000`));
+
+
+
+// Enable CORS for all routes and methods
+app.use(cors({
+  origin: 'http://localhost:3000', // Replace with your frontend URL
+  methods: ['GET', 'POST'],
+  allowedHeaders: ['Content-Type']
+}));
+
 const server = http.createServer(app);
-const io = new Server(server);
+const io = new Server(server, {
+  cors: {
+    origin: "http://localhost:3000", // Replace with your frontend URL
+    methods: ["GET", "POST"]
+  }
+});
 
 io.on("connection", (socket) => {
   socket.on("chating-message", (data) => {
@@ -14,10 +47,8 @@ io.on("connection", (socket) => {
   });
 });
 
-app.use(express.static(path.resolve("./src")));
+// If you still have static files to serve for other purposes, use the following line
+// app.use(express.static(path.resolve("./src")));
 
-app.get("/", (req, res) => {
-  return res.sendFile("/src/index.html");
-});
-
-server.listen(3000, () => console.log(`Server Started at PORT:3000`));
+// Start the server on port 5000
+server.listen(5000, () => console.log(`Server Started at PORT: 5000`));
